759873d37dd6f4cd804d715b41281ac6bbaed43f
学渣驹 <liaozecheng123@163.com>
2024-08-01 07:32:52 +0800

    Add files via upload

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,7 +2,9 @@ extends Node
 
 var score = 0
 @onready var score_label = $ScoreLabel
+@onready var Score = $"../Playerf/Panel/Score"
 
 func add_point():
 	score += 1
 	score_label.text = "Great Job!You made it!You collected" + str(score) + "coins!"
+	Score.text = "Score:" + str(score)
